824d234f3f3f3cab97232e2e7f1c7275873f3bb3
adarsh ramgirwar <adarshramgirwar1@gmail.com>
2024-06-30 11:50:18 +0530

    frontend setup ready

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-xl text-center pt-[10%] font-bold underline">project configure with tailwind css and prettier </h1>
+    </>
+  );
+}
+
+export default App;
