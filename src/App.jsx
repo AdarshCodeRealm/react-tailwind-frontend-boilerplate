766299423e6b02cfca07294cd02706c358e29853
adarsh ramgirwar <adarshramgirwar1@gmail.com>
2024-07-14 09:26:19 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ function App() {
   return (
     <>
       <h1 className="text-xl text-center pt-[10%] font-bold underline">project configure with tailwind css and prettier </h1>
-    </>
+      <h1>router</h1>
+   </>
   );
 }
 
